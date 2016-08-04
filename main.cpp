63744e19c016c3cfc38166ece23a37186441c39b
mandarynian <kortower@gmail.com>
2016-08-04 11:49:13 +0200

    jipp3
    Projekt na potrzeby zajęć z przedmiotu JIPP (PK)

--- main.cpp
+++ main.cpp
@@ -1,50 +1,104 @@
-// funkcja g³ówna 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-#include "naglowek.h"
-#include <cstdio>
+
+/*int losowanie(int **t, int i, int j, int max, int min) {
+	int rmk, rmw, rnw, rnk;
+losowanie:
+	rmw = rand() % i;
+	rmk = rand() % j;
+
+	rnw = rand() % i;
+	rnk = rand() % j;
+	if (rnk == rmk) {
+		goto losowanie;
+	}
+	t[rmw][rmk] = max;
+	t[rnw][rnk] = min;
+}*/
+
+int wymiana(int **t, int w, int k) {
+	int pom = 0, kmin = 0, kmax = 0, pommax = 0, pommin = 0;
+	pommax = t[0][0];
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			pom = t[i][j];
+			if (pommax < pom) {
+				pommax = pom;
+				kmax = j;
+			}
+		}
+	}
+	pommin = t[0][0];
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			pom = t[i][j];
+			if (pommin > pom) {
+				pommin = pom;
+				kmin = j;
+			}
+		}
+	}
+
+	for (int i = 0; i < w; i++) {
+		pom = t[i][kmax];
+		t[i][kmax] = t[i][kmin];
+		t[i][kmin] = pom;
+	}
+
+	printf("kmax %d wmax %d min %d pommin %d\n", kmax, pommax, kmin, pommin);
+
+}
 
 int main() {
 
-	cout << "Sortowanie quciksort losowych liczb z przedzialu <-100,100>" << endl;
-	cout << "Podaj liczbe elemntow tablicy: ";
-	cin >> ile;
+	int **tablica = NULL, w, k;
+	int max = 30, min = -30, rmw, rmk, rnw, rnk;
+	int pom = 0, pommin = 0, pommax = 0, kakao = 0, makao = 0;
+	int kmax = 0, kmin = 0;
 
-	//alokacja pamiêci dla dwóch tablic 
-	tab = new int[ile];
-	tab2 = new int[ile];
+	printf("Podaj liczbe kolumn i wierszy");
+	scanf("%d %d", &k, &w);
 
-	//wype³nianie tablicy tab losowymi liczbami z przedzia³u <-100,100>
-	srand((time(NULL)));
-	for (int i = 0; i < ile; i++) {
-		tab[i] = rand() % 200 - 101;
+	srand(time(NULL));
+
+	tablica = (int **)malloc(w * sizeof(int *));
+	for (int i = 0; i < w; i++) {
+		tablica[i] = (int*)malloc(k * sizeof(int));
+		for (int j = 0; j<k; j++) {
+			tablica[i][j] = rand() % 20 * pow(-1,rand()%2);
+		}
 	}
 
-	//kopiowanie zawartoœci tablicy tab do tablicy tab2
-	for (int j = 0; j < ile; j++) {
-		tab2[j] = tab[j];
-	}
+	printf("Dane wygenerowane losowo z przedzialu od 0 do 20\n");
+	for (int i = 0; i < w; ++i, printf("\n"))
+		for (int j = 0; j < k; ++j)
+			printf("%.1d\t", tablica[i][j]);
 
-	//sortowanie liczb w³aœciwe
-	cout << endl << "Sortowanie QuickSort wlasciwe..." << endl;
-	start = clock();
-	qs(tab, 0, ile - 1);
-	stop = clock();
-	czas1 = (double)(stop - start) / CLOCKS_PER_SEC;
-	printf("Czas potrzebny na posortowanie tablicy w sposob wlasciwy: %.9f \n", czas1);
+	//losowanie(tablica, w, k, 30, -30);
 
-	//sortowanie liczb odwrotne
-	cout << endl << "Sortowanie QucikSort odwrotne..." << endl;
-	start2 = clock();
-	qs_o(tab2, 0, ile - 1);
-	stop2 = clock();
-	czas2 = (double)(stop2 - start2) / CLOCKS_PER_SEC;
-	printf("Czas potrzebny na posortowanie tablicy w sposob odwrotny: %.9f \n", czas2);
+	printf("Dane po wlosowaniu do nich liczby najwiekszej i najmniejszej\n");
+	for (int i = 0; i < w; ++i, printf("\n"))
+		for (int j = 0; j < k; ++j)
+			printf("%.1d\t", tablica[i][j]);
 
-	cout << endl;
+	wymiana(tablica, w, k);
 
-	//zwolnienie pamiêci 
-	delete[] tab2;
-	delete[] tab;
+	printf("Zmiana kolumn miejscami z najmniejszya i najwieksza liczba\n");
+	for (int i = 0; i < w; ++i, printf("\n"))
+		for (int j = 0; j < k; ++j)
+			printf("%.1d\t", tablica[i][j]);
 
+	for (int i = 0; i<w; i++)
+		free(tablica[i]);
+	free(tablica);
+
+	system("pause");
 	return 0;
 }
