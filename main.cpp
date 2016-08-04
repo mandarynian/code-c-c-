f8cb0f9a9800a3d9d17ba0fa6e8469e66397f12a
mandarynian <kortower@gmail.com>
2016-08-04 11:50:43 +0200

    jipp2
    Projekt na potrzeby zajęć z przedmiotu JIPP (PK)

--- main.cpp
+++ main.cpp
@@ -1,104 +1,67 @@
+#pragma warning (disable: 4996)
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
+#define MAX_LINE 256
+void liczymyznaki(int *c, char *dd, int *j, int *jolo);
+//void alokujpamiec(int g,char **ma, int *schabowy);
 
-
-/*int losowanie(int **t, int i, int j, int max, int min) {
-	int rmk, rmw, rnw, rnk;
-losowanie:
-	rmw = rand() % i;
-	rmk = rand() % j;
-
-	rnw = rand() % i;
-	rnk = rand() % j;
-	if (rnk == rmk) {
-		goto losowanie;
-	}
-	t[rmw][rmk] = max;
-	t[rnw][rnk] = min;
-}*/
-
-int wymiana(int **t, int w, int k) {
-	int pom = 0, kmin = 0, kmax = 0, pommax = 0, pommin = 0;
-	pommax = t[0][0];
-	for (int i = 0; i < w; i++)
-	{
-		for (int j = 0; j < k; j++)
-		{
-			pom = t[i][j];
-			if (pommax < pom) {
-				pommax = pom;
-				kmax = j;
-			}
-		}
-	}
-	pommin = t[0][0];
-	for (int i = 0; i < w; i++)
-	{
-		for (int j = 0; j < k; j++)
-		{
-			pom = t[i][j];
-			if (pommin > pom) {
-				pommin = pom;
-				kmin = j;
-			}
-		}
-	}
-
-	for (int i = 0; i < w; i++) {
-		pom = t[i][kmax];
-		t[i][kmax] = t[i][kmin];
-		t[i][kmin] = pom;
-	}
-
-	printf("kmax %d wmax %d min %d pommin %d\n", kmax, pommax, kmin, pommin);
-
-}
 
 int main() {
+	char d[MAX_LINE], *dd, b, p = 'a';
+	char **macierz;
+	int j = -1, c = 0, i;
+	int kupa = 0, kibel = 0;
+	int jolo[MAX_LINE];
 
-	int **tablica = NULL, w, k;
-	int max = 30, min = -30, rmw, rmk, rnw, rnk;
-	int pom = 0, pommin = 0, pommax = 0, kakao = 0, makao = 0;
-	int kmax = 0, kmin = 0;
+	dd = gets(d);
+	liczymyznaki(&c, dd, &j, (int *)&jolo);
 
-	printf("Podaj liczbe kolumn i wierszy");
-	scanf("%d %d", &k, &w);
 
-	srand(time(NULL));
+	int ziemniak;
+	macierz = (char**)malloc(j * sizeof(char *));
 
-	tablica = (int **)malloc(w * sizeof(int *));
-	for (int i = 0; i < w; i++) {
-		tablica[i] = (int*)malloc(k * sizeof(int));
-		for (int j = 0; j<k; j++) {
-			tablica[i][j] = rand() % 20 * pow(-1,rand()%2);
-		}
+
+	for (int h = 0; h < j; h++) {
+		ziemniak = jolo[h];
+		macierz[h] = (char*)malloc(ziemniak * sizeof(char *));
 	}
 
-	printf("Dane wygenerowane losowo z przedzialu od 0 do 20\n");
-	for (int i = 0; i < w; ++i, printf("\n"))
-		for (int j = 0; j < k; ++j)
-			printf("%.1d\t", tablica[i][j]);
 
-	//losowanie(tablica, w, k, 30, -30);
 
-	printf("Dane po wlosowaniu do nich liczby najwiekszej i najmniejszej\n");
-	for (int i = 0; i < w; ++i, printf("\n"))
-		for (int j = 0; j < k; ++j)
-			printf("%.1d\t", tablica[i][j]);
+	while ((b = *dd) != '\0') {
 
-	wymiana(tablica, w, k);
+		if (b != ' ') {
+			macierz[kupa][kibel] = b;
+			if (p == ' ')
+				kupa++;
+		}
+		p = b;
+		kibel++;
+		dd++;
+	}
 
-	printf("Zmiana kolumn miejscami z najmniejszya i najwieksza liczba\n");
-	for (int i = 0; i < w; ++i, printf("\n"))
-		for (int j = 0; j < k; ++j)
-			printf("%.1d\t", tablica[i][j]);
-
-	for (int i = 0; i<w; i++)
-		free(tablica[i]);
-	free(tablica);
+	for (i = 0; i < j; i++) {
+		printf("%s \n", macierz[i]);
+	}
 
 	system("pause");
 	return 0;
+}
+
+void liczymyznaki(int *c, char *dd, int *j, int *t) {
+	char b, p = ' ';
+	int gargamel = 0;
+	while (b = *dd, b != '\0') {
+		if (b != ' ') {
+			c++;
+			if (p == ' ') {
+				*j += 1;
+				t[gargamel] = (int)c;
+				c = 0;
+				gargamel++;
+			}
+		}
+		p = b;
+		dd++;
+	}
 }
